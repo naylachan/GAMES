8556191b16e9f29c5a849378d72da5a518b02b29
Naylachan <82255308+naylachan@users.noreply.github.com>
2022-09-24 06:41:01 +0800

    Add files via upload

---
+++ matematika.js
@@ -0,0 +1,174 @@
+[
+ {
+  "soal": "Nilai x yang memenuhi persamaan | x + 5 | = 3 …..",
+  "jawaban": "x = -2 dan x = -8"
+ },
+ { 
+ "soal": "penyelesaian dari √(2x+6)>0 adalah …..",
+ "jawaban": "x>-3"
+ },
+ { 
+ "soal": "Perhatikan himpunan berikut ini!\n\n2x+4y-2z=-4\n-2x-y+3z=2\nx+3y+4z=2)\n\nHimpunan penyelesaian yang memenuhi sistem persamaan di bawah ini adalah...",
+ "jawaban": "{1,-1,1}"
+ },  
+ { 
+ "soal": "Diketahui f(x) = 2 – x dan g(x) = x² + 5x, fungsi komposisi (g o f) (x) adalah …..",
+ "jawaban": "x² – 9x + 14"
+ },
+ { 
+ "soal": "Suatu pesawat udara mempunyai 50 tempat duduk penumpang. Setiap penumpang kelas utama boleh membawa 60 kg barang, sedangkan untuk penumpang kelas ekonomi boleh membawa 20 kg barang. Pesawat tersebut hanya dapat membawa 1500 kg barang. Jika banyak penumpang kelas utama adalah x dan banyaknya kelas ekonomi y, model matematika yang harus dipenuhi oleh x dan y adalah..........",
+ "jawaban": "x≥0, x+y≤50, 3x+y≤75"
+ },
+ { 
+ "soal": "Nilai maksimum f(x,y) = 5x + 4y yang memenuhi pertidaksamaan x+y≤8, x+2y≤12, x≥0, y≥0 adalah ….",
+ "jawaban": "40"
+ },
+ { 
+ "soal": "Diketahui koordinat P(-8, 12) dilatasi [ P,1] memetakan titik (-4, 8) ke titik.....",
+ "jawaban": "(-4, 8)"
+ },
+ { 
+ "soal": "Nilai suku ke 6 dan suku ke 14 suatu barisan aritmatika berturut-turut 3 dan 19, nilai suku ke 30 adalah ....",
+ "jawaban": "51"
+ },
+ { 
+ "soal": "Nilai suku ke 3 dan suku ke 6 suatu deret geometri -4 dan 32 jumlah Sembilan suku pertama deret tersebut adalah . . .",
+ "jawaban": "-171"
+ },
+ { 
+ "soal": "Nilai turunan pertama fungsi f(x) = 2x³ + 3x² – 12x + 5 untuk x = -4 adalah…",
+ "jawaban": "60"
+ },
+ { 
+ "soal": "Diketahui fungsi g(x)=-x³ + 6x² - 9x + 15. Grafik fungsi g turun pada nilai …",
+ "jawaban": "x < 1 atau x > 3"
+ },
+ { 
+ "soal": "Biaya produksi x unit barang dalam ribuan rupiah dirumuskan p(x) = x² + 8x + 20. Harga jual barang tersebut 40 ribu per unit, keuntungan maksimum yang diperoleh sebesar ….",
+ "jawaban": "Rp. 236.000,-"
+ },
+ { 
+ "soal": "Empat kelompok siswa yang masing-masing terdiri dari 5,8,10, dan 17 orang menyumbang korban bencana alam. Rata-rata sumbangan masing-masing kelompok adalah Rp 4.000,00, Rp2.500,00, dan Rp2.000,00, Rp1.000,00. Maka rata-rata sumbangan tiap siswa seluruh kelompok adalah …",
+ "jawaban": "Rp 1.925,00"
+ },
+ { 
+ "soal": "Dari 10 orang finalis suatu lomba kecantikan akan dipilih secara acak 3 yang terbaik. Banyak cara pemilihan tersebut adalah …",
+ "jawaban": "120"
+ },
+ { 
+ "soal": "Suatu kepanitiaan terdiri dari 3 pria dan 2 wanita. Jika banyak siswa yang diusulkan untuk duduk dalam kepanitiaan ada 7 pria dan 9 wanita, banyak susunan panitia yang dapat dibentuk adalah …",
+ "jawaban": "1.260"
+ },
+ { 
+ "soal": "Dari 6 orang calon pengurus suatu organisasi akan dipilih menjadi ketua, wakil ketua, sekretaris dan bendahara yang masing-masing satu orang. Banyaknya susunan pengurus yang dapat terbentuk adalah …",
+ "jawaban": "360"
+ },
+ { 
+ "soal": "Nomor pegawai pada suatu pabrik terdiri atas tiga angka denagn angka pertama tidak nol. Banyak nomor pegawai yang ganjil adalah ….",
+ "jawaban": "450"
+ },
+ { 
+ "soal": "Diketahui kubus ABCD,.EFGH, rusuk-rusuknya 10 cm. jarak titik F ke garis AC adalah …",
+ "jawaban": "5√6"
+ },
+ { 
+ "soal": "Sebuah dompet berisi 5 keping uang logam lima ratusn rupiah dan 2 keping ratusan rupiah. Dompet yang lain berisi 1 keping uang logam lima ratusan dan 3 keping ratusan rupiah. Jika sebuah uang logam diambil secara acak dari salah satu dompet, maka peluang untuk mendapatkan uang logam ratusan rupiah adalah …",
+ "jawaban": "6/28"
+ },
+ { 
+ "soal": "Panjang hasil proyeksi vektor v pada vektor w adalah 3 satuan, dengan sudut antara v dan w sebesar 45°. Jika |w |=3, maka |v |= ....",
+ "jawaban": "3√2"
+ },
+ { 
+ "soal": "Vektor a diproyeksikan pada vektor b. Keterangan yang benar berikut ini tentang hasil proyeksi vektor ortoghonal yang terbentuk, jika c adalah hasil proyeksinya adalah ….",
+ "jawaban": "vektor satuan c sama dengan vektor satuan b"
+ },
+ { 
+ "soal": "Turunan pertama dari fungsi f(x) = 5 cos (2x - 1) adalah... .",
+ "jawaban": "f '(x) = - 10 sin (2x - 1)"
+ },
+ { 
+ "soal": "Panjang vektor a dan b masing-masing adalah 6 satuan dan 8 satuan. Jika besar sudut antara kedua vektor sama dengan 60°, maka I a - b I adalah ....",
+ "jawaban": "2√13"
+ },
+ { 
+ "soal": "Hasil dari ∫ (2x + 3)² dx = ...",
+ "jawaban": "4/3x³ + 6x² + 9x + c"
+ },
+ { 
+ "soal": "Suku banyak F(x) dibagi (x + 1) sisa 3 dan jika dibagi (x -2) sisa 6. Sisa pembagian suku banyak F(x) oleh x2 –x – 2 adalah ….",
+ "jawaban": "x + 4"
+ },
+ { 
+ "soal": "Langkah pertama pembuktian suatu deret dengan induksi matematika untuk n bilangan asli adalah ...",
+ "jawaban": "buktikan benar untuk n = 1"
+ },
+ { 
+ "soal": "Seorang pedagang teh mempunyai etalase yang hanya cukup ditempati 30 box teh. Teh A dibeli dengan harga Rp 6.000,00 setiap box dan teh B dibeli dengan dengan harga Rp 8.000,00 setiap box. Jika pedangan tersebut mempunyai modal Rp 300.000,00 untuk membeli x box teh A dan y box teh B. Sistem pertidaksamaan dari permasalahan tersebut adalah ….",
+ "jawaban": "3x + 4y ≤ 150, x + y ≤ 30, x ≥ 0, y ≥ 0"
+ },
+ { 
+ "soal": "Jumlah 200 bilangan asli pertama adalah ...",
+ "jawaban": "20.100"
+ },
+ { 
+ "soal": "Bidang-bidang yang berpotongan tegak lurus pada kubus ABCD.EFGH adalah ...",
+ "jawaban": "ACGE dengan BDHF"
+ },
+ { 
+ "soal": "Tujuh tahun yang lalu umur ayah sama dengan 6 kali umur Budi. Empat tahun yang akan\ndatang 2 kali umur ayah sama dengan 5 kali umur Budi ditambah 9 tahun. Umur ayah\nsekarang adalah ........",
+ "jawaban": "43 tahun"
+ },
+ { 
+ "soal": "Nilai x yang memenuhi pertidaksamaan :\n2 log x  log (2x + 5) + 2 log 2 adalah ........",
+ "jawaban": " -2 < x < 0"
+ },
+ { 
+ "soal": "Persamaan lingkaran yang berpusat di (1, 4) dan menyinggung garis 3x - 4y - 2 = 0\nadalah........",
+ "jawaban": "x² + y² - 2x - 8y + 8 = 0"
+ },
+ { 
+ "soal": "Salah satu persamaan garis singgung lingkaran x² + y² = 25 yang tegak lurus garis 2y - x +\n3 = 0 adalah ........",
+ "jawaban": "y = -2x + 5"
+ },
+ { 
+ "soal": "Nilai x yang memenuhi persamaan 2 cos²x - 2 sin x . cos x - 1 -  = 0,",
+ "jawaban": "45°, 105°, 225°, 285°"
+ },
+ { 
+ "soal": "Seutas tali dipotong menjadi 7 bagian dan panjang masing-masing potongan membentuk\nbarisan geometri. Jika panjang potongan tali terpendek sama dengan 6 cm dan panjang\npotongan tali terpanjang sama dengan 384 cm, panjang keseluruhan tali tersebut\nadalah........",
+ "jawaban": "762 cm"
+ },
+ { 
+ "soal": "Seorang anak menabung di suatu bank dengan selisih kenaikan tabungan antar bulan tetap.\nPada bulan pertama sebesar Rp 50.000,00, bulan kedua Rp 55.000,00, bulan ketiga Rp\n60.000,00, dan seterusnya.\nBesar tabungan anak tersebut selama 2 tahun adalah ........",
+ "jawaban": "Rp 2.580.000,00"
+ },
+ { 
+ "soal": "Diketahui A(1, 2, 3), B(3, 3, 1), dan C(7, 5, -3). Jika A, B, dan C segaris (kolinier),\nperbandingan  = ........",
+ "jawaban": "1 : 2"
+ },
+ { 
+ "soal": "Persamaan peta suatu kurva oleh rotasi pusat O bersudut  , dilanjutkan dilatasi (0, 2)\nadalah x = 2 + y - y². Persamaan kurva semula adalah ........",
+ "jawaban": " y = 2x² - x - 1"
+ },
+ { 
+ "soal": "Suatu perusahaan menghasilkan produk yang dapatdiselesaikan dalam x jam, dengan\nbiaya per jam (4x - 800 +  ) ratus ribu rupiah . Agar biaya minimum, produk tersebut\ndapat diselesaikan dalam waktu ........",
+ "jawaban": "100 jam"
+ },
+ { 
+ "soal": "Persamaan gerak suatu partikel dinyatakan dengan rumus x = f(t) =  (s dalam\nmeter dan t dalam detik). Kecepatan partikel pada saat t = 8 detik adalah ........",
+ "jawaban": "m/detik"
+ },
+ { 
+ "soal": "Turunan dari F(x) =  adalah F '(x) = ........",
+ "jawaban": "- (6x + 5) tan(3x² + 5x)"
+ },
+ { 
+ "soal": "Tanah seluas 10.000 m² akan dibangun rumah tipeA dan tipe B. Untuk rumah tipe A\ndiperlukan 100 m² dan tipe B diperlukan 75 m². Jumlah rumah yang dibangun paling\nbanyak 125 unit. Keuntungan rumah tipe A adalah Rp 6.000.000,00/unit dan tipe B adalah\nRp 4.000.000,00/unit. Keuntungan maksimum yang dapat diperoleh dari penjualan rumah\ntersebut adalah ........",
+ "jawaban": "Rp 600.000.000,00"
+ },
+ { 
+ "soal": "Diketahui premis-premis berikut :\n1. Jika Budi rajin belajar maka ia menjadi pandai.\n2. Jika Budi menjadi pandai maka ia lulus ujian.\n3. Budi tidak lulus ujian.\nKesimpulan yang sah adalah ........",
+ "jawaban": "Budi tidak rajin belajar"
+ } 
+]
